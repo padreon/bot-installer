ffb7154ac001538454c03379ee3d8575804199f7
padreon <xaxakai21@gmail.com>
2017-10-25 20:57:12 +0800

    Create alphat.sh

---
+++ alphat.sh
@@ -0,0 +1,6 @@
+sudo apt-get install python-software-properties;
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -;
+sudo apt-get install nodejs;
+sudo apt-get install git;
+sudo apt-get install screen;
+echo silahkan tulis git clone github.com/alfathdirk/LineAlphatJS.git;
